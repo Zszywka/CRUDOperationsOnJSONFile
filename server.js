4404a90769302166d8c5a2cf6542e59edc95e907
Zszywka <32071894+Zszywka@users.noreply.github.com>
2018-07-15 00:23:17 +0200

    Delete the comments

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-// zaleznosci pobrane:
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs')
-// deklaracja zmiennej app
+
 var app = express();
 var stringifyFile;
 
-// pozwala uzyc body-parser(pozwala korzysta z formatu application/json)
 app.use(bodyParser.json());
 
 app.get('/getNote', function(req, res) {
-  // ta funkcja wywola sie po zaladowaniu
   fs.readFile('./test.json', 'utf-8', function(err, data) {
     if (err) throw err;
     stringifyFile = data;
@@ -18,9 +15,8 @@ app.get('/getNote', function(req, res) {
   });
 });
 
-//funkcja nadpisujaca zawartosc test.json (localhost:3000/updateNote/test)
 app.post('/updateNote/:note', function(req, res) {
-  stringifyFile += req.params.note //?dlaczego tu a nie w fs.writeFile?
+  stringifyFile += req.params.note //?nie w fs.writeFile?
 
   fs.writeFile('./test2.json', stringifyFile, function(err) {
     if (err) throw err;
