15260dbce11c6309b3f0a7ea4f525e88e536379a
Zszywka <32071894+Zszywka@users.noreply.github.com>
2018-07-15 21:36:45 +0200

    overwrite the file test.json
    overwrite the file using the POST and the writeFile method

--- server.js
+++ server.js
@@ -17,11 +17,14 @@ app.get('/getNote', function(req, res) {
 
 app.post('/updateNote/:note', function(req, res) {
   stringifyFile += req.params.note //?nie w fs.writeFile?
-
-  fs.writeFile('./test2.json', stringifyFile, function(err) {
+  // console.log(stringifyFile);
+  // console.log("Teraz jest w POST i stringiFIle to:" + stringifyFile);
+  fs.writeFile('./test.json', stringifyFile, function(err) {
     if (err) throw err;
     console.log('file updated');
+    // console.log('file updated');
     res.send(stringifyFile);
+    // console.log(stringifyFile);
   })
 })
 
